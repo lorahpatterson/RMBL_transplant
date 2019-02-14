7728d651ba5147cbb954015206fd691c04e6aa1e
lorahpatterson <lorahbeth@gmail.com>
2019-02-14 13:13:43 -0800

    Starting script

---
+++ phenology/RMBL_phenology.R
@@ -0,0 +1,9 @@
+# RMBL transplant phenology paper
+# February 14, 2019
+# Lorah Seltzer
+
+
+###################################
+# Figure 1. How does the phenology of each species shift in response to warming and earlier snowmelt?
+
+# Graph julian day of year vs flower abundance for each species and treatment.
